9110f255f3e4be433305e3e2e1c9b3bc22e576e3
ohiayame <0819aym001@gmail.com>
2026-02-02 21:45:55 +0900

    js 특강 2

---
+++ course-js/test/2.js
@@ -0,0 +1,32 @@
+// let myList = [10, 20, 30];
+
+// for (value of myList) {
+//     console.log(value);
+// }
+
+// for (let i = 0; i < myList.length; i++) {
+//     console.log(myList[i]);
+// }
+
+let myList1 = [100, 200, 300];
+myList1[5] = 20;
+for (let i = 0; i < myList1.length; i++) {
+  console.log(myList1[i]);
+}
+
+console.log(myList1.length === Object.keys(myList1).length);
+// console.log(myList2[1], typeof myList2);
+
+// let myMap = new Map();
+
+// myMap.set(10, "A");
+// myMap.set(1, "B");
+
+// console.log(myMap.get(10));
+// console.log(myMap.get(1));
+
+// let mySet = new Set()
+// while (mySet.size < 6){
+//     random생성
+//     mySet.add(random)
+// }
