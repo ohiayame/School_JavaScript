9a01731b22f9aa0201ca6b98149307165ef5e833
ohiayame <0819aym001@gmail.com>
2026-02-03 22:21:12 +0900

    js 특강 3

--- course-js/test/2.js
+++ course-js/test/2.js
@@ -8,13 +8,13 @@
 //     console.log(myList[i]);
 // }
 
-let myList1 = [100, 200, 300];
-myList1[5] = 20;
-for (let i = 0; i < myList1.length; i++) {
-  console.log(myList1[i]);
-}
+// let myList1 = [100, 200, 300];
+// myList1[5] = 20;
+// for (let i = 0; i < myList1.length; i++) {
+//   console.log(myList1[i]);
+// }
 
-console.log(myList1.length === Object.keys(myList1).length);
+// console.log(myList1.length === Object.keys(myList1).length);
 // console.log(myList2[1], typeof myList2);
 
 // let myMap = new Map();
@@ -30,3 +30,68 @@ console.log(myList1.length === Object.keys(myList1).length);
 //     random생성
 //     mySet.add(random)
 // }
+
+// let myList = [10, 20, 30, 40];
+
+// 1) 출력
+// for (let i = 0; i < myList.length; i++) {
+//   console.log(myList[i]);
+// }
+
+// // 2) +1
+// for (let i = 0; i < myList.length; i++) {
+//   myList[i] += 1;
+// }
+// // 3) 출력
+// for (let i = 0; i < myList.length; i++) {
+//   console.log(myList[i]);
+// }
+
+// // 4) +2
+// for (let i = 0; i < myList.length; i++) {
+//   myList[i] += 2;
+// }
+// // 5) 출력
+// for (let i = 0; i < myList.length; i++) {
+//   console.log(myList[i]);
+// }
+
+// function
+// function forEach(argArray) {
+//   for (let i = 0; i < argArray.length; i++) {
+//     console.log(argArray[i]);
+//   }
+// };
+
+// function map(argArray, argAddValue) {
+//   for (let i in argArray) {
+//     argArray[i] += argAddValue;
+//   }
+// }
+
+// function map(argArray, argFn) {
+//   for (let i in argArray) {
+//     argArray[i] = argFn(argArray[i]);
+//   }
+// }
+
+// function forEach(argArray, argFn) {
+//   for (let i = 0; i < argArray.length; i++) {
+//     argFn(argArray[i]);
+//   }
+//   console.log();
+// }
+
+// forEach(myList, (v) => process.stdout.write(`${v}\t`));
+// forEach(myList, (v) => console.log(`${v}\t`));
+// myList.forEach((v) => console.log(`${v}\t`));
+// // map(myList, v => v + 1);
+// myList = myList.map((v) => {
+//   return v + 1;
+// });
+// myList.forEach((v) => console.log(`${v}\t`));
+// // map(myList, (v) => v + 2);
+// myList = myList.map((v) => v + 2);
+// myList.forEach((v) => console.log(`${v}\t`));
+
+
---
+++ course-js/test/3.js
@@ -0,0 +1,60 @@
+// let myList = [10, 20, 30];
+
+// // forEach 반복 -> 함수 호출
+// function forEach(argArray, argFn) {
+//   // 반복
+//   for (let i = 0; i < argArray.length; i++) {
+//     // 호출
+//     argFn(argArray[i]);
+//   }
+// }
+
+// // map 반복 -> 함수 호출 반환값 저장
+// function map(argArray, argFn) {
+//   // 반복
+//   for (let i = 0; i < argArray.length; i++) {
+//     // 함수 호출 => 반환값 저장
+//     argArray[i] = argFn(argArray[i]);
+//   }
+// }
+
+// // 1) forEach 호출 -> 각 값 출력
+// forEach(myList, (v) => console.log(v));
+
+// // 2) map 호출 -> 각 값 에 +1
+// map(myList, (v) => v + 1);
+// // forEach 호출 -> 각 값 출력
+// forEach(myList, (v) => console.log(v));
+
+// // 3) map 호출 -> 각 값 에 +1
+// map(myList, (v) => v + 2);
+// // forEach 호출 -> 각 값 출력
+// forEach(myList, (v) => console.log(v));
+
+// myList.forEach((v) => console.log(v));
+// myList = myList.map((v) => v + v);
+// myList.forEach((v) => console.log(v));
+
+// for (let index = 0; index < 3; index++) {
+//   myList = myList.map((v) => v + index);
+//   myList.forEach((v) => console.log(v));
+// }
+
+let myList = [10, 299, 30, 400];
+
+function filter(argList, argFn) {
+    let newList = []
+    // 배열을 반복하고 조건에 맞으면 새로운 리스트에 추가
+    for (let index = 0; index < argList.length; index++) {
+        if (argFn(argList[index])) {
+          newList.push(argList[index]);
+        }
+    }
+    // 새로운 배열 반환
+    return newList
+}
+
+// 100이상 값들 만 리스트로 생성
+// let newLi1 = myList.filter((v) => v >= 100);
+
+console.log(filter(myList, (v) => v >= 100));
